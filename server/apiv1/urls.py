3584d8c8020b05c1fc98f398e732499843b82476
guravity <shogo.ogura365@gmail.com>
2022-11-03 12:47:12 +0900

    Change APIView>ModelViewSet

--- server/apiv1/urls.py
+++ server/apiv1/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url, include
+from rest_framework import routers
 
 app_name = 'apiv1'
+
+router = routers.DefaultRouter()
+router.register(r'books', views.BookViewSet)
+
 urlpatterns = [
-    path('categories/', views.CategoryListAPIView.as_view()),
-    path('books/', views.BookListAPIView.as_view()),
+    path('categories-list/', views.CategoryListAPIView.as_view()),
+    path('books-list/', views.BookListAPIView.as_view()),
+    url(r'', include(router.urls))
 ]
--- server/apiv1/views.py
+++ server/apiv1/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets, filters
 from .models import Book, Category
 from .serializers import CategorySerializer, BookSerializer
 
@@ -7,8 +7,21 @@ class CategoryListAPIView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-
 class BookListAPIView(generics.ListAPIView):
     """本モデルの取得（一覧）APIクラス"""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  filter_backends = [
+    filters.SearchFilter,
+    filters.OrderingFilter,
+    #CategoryFilter,
+  ]
+  search_fields = ('title', 'content')
+  ordering_fields = ('created_at', 'updated_at')
+
+#   def perform_create(self, serializer):
+#     serializer.save(user=self.request.user)
